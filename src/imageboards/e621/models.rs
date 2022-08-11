dd8058f1b6211b833bb2b1b40d9e3c361acdca29
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-11 08:04:00 -0300

    begin e621 implementation

---
+++ src/imageboards/e621/models.rs
@@ -0,0 +1,18 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct E621TopLevel {
+    pub posts: Vec<E621Post>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct E621Post {
+    pub file: E621File
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct E621File {
+    pub ext: String,
+    pub md5: String,
+    pub url: String
+}
