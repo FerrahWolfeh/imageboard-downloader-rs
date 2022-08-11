57368d51dde9b8a81e28630f0803cd844a7da8bf
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-11 08:01:42 -0300

    remove unused field in danbooru model

--- src/imageboards/danbooru/models.rs
+++ src/imageboards/danbooru/models.rs
@@ -2,7 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct DanbooruItem {
-    pub file_size: u64,
     pub md5: Option<String>,
     pub file_ext: Option<String>,
     pub file_url: Option<String>,
