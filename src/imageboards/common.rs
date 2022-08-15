fcc253403e1ce81ef912cea877a36c112dee4dc5
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-15 07:32:17 -0300

    [general] misplaced commit

--- src/imageboards/common.rs
+++ src/imageboards/common.rs
@@ -108,7 +108,6 @@ impl CommonPostItem {
                 bail!("")
             }
         } else {
-            main_bar.set_length(main_bar.length().unwrap() - 1);
             Ok(())
         }
     }
