0a55c0d771b10e6b1a49e6ee0abf279916a6b0de
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-18 07:25:15 -0300

    [general] remove useless commented code

--- src/imageboards/mod.rs
+++ src/imageboards/mod.rs
@@ -166,52 +166,6 @@ impl ImageBoards {
     }
 }
 
-// impl fmt::Display for ImageBoards {
-//     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-//         fmt.pad(self.as_str())
-//     }
-// }
-//
-// impl ImageBoards {
-//     pub fn as_str(&self) -> &'static str {
-//         IMAGEBOARD_NAMES[*self as usize]
-//     }
-//
-//     fn from_usize(u: usize) -> Option<ImageBoards> {
-//         match u {
-//             1 => Some(ImageBoards::Danbooru),
-//             2 => Some(ImageBoards::E621),
-//             3 => Some(ImageBoards::Rule34),
-//             4 => Some(ImageBoards::RealBooru),
-//             _ => None,
-//         }
-//     }
-// }
-//
-// impl FromStr for ImageBoards {
-//     type Err = ParseError;
-//
-//     fn from_str(imgboard: &str) -> Result<ImageBoards, Self::Err> {
-//         IMAGEBOARD_NAMES
-//             .iter()
-//             .position(|&name| str::eq_ignore_ascii_case(name, imgboard))
-//             .map(|p| ImageBoards::from_usize(p).unwrap())
-//             .ok_or(ParseError(()))
-//     }
-// }
-//
-// #[allow(missing_copy_implementations)]
-// #[derive(Debug, PartialEq)]
-// pub struct ParseError(());
-//
-// impl fmt::Display for ParseError {
-//     fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-//         fmt.write_str(IMAGEBOARD_PARSE_ERROR)
-//     }
-// }
-//
-// impl error::Error for ParseError {}
-
 #[cfg(test)]
 mod tests {
     use crate::imageboards::common::generate_out_dir;
