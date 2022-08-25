ed506c22d7cc2767b769901723c71b1dda1de1db
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-25 14:44:39 -0300

    draft of the blacklist decoder

--- src/imageboards/blacklist.rs
+++ src/imageboards/blacklist.rs
@@ -1,19 +1,30 @@
+use ahash::AHashSet;
 use anyhow::Error;
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
+use tokio::fs::read_to_string;
+use toml::from_str;
+use xdg::BaseDirectories;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct GlobalBlacklist {
     /// In this array, the user will declare tags that should be excluded from all imageboards
-    global_blacklist: Vec<String>,
+    global_blacklist: AHashSet<String>,
 }
 
 impl GlobalBlacklist {
-    pub fn get_gbl() -> Result<Self, Error> {
-        todo!()
+    pub async fn get() -> Result<Option<Self>, Error> {
+        if let Ok(gbl) = read_to_string(Self::path()?).await {
+            let deserialized = from_str::<Self>(&gbl)?;
+            return Ok(Some(deserialized));
+        }
+        Ok(None)
     }
 
-    fn path() -> PathBuf {
-        todo!()
+    fn path() -> Result<PathBuf, Error> {
+        let xdg_dir = BaseDirectories::with_prefix("imageboard-downloader")?;
+
+        let dir = xdg_dir.place_config_file("blacklist.toml")?;
+        Ok(dir)
     }
 }
