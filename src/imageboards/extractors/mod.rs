af00924248bc123d67fa08e41d3a5f2763658209
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-26 13:49:38 -0300

    [refactor] Change format of trait

--- src/imageboards/extractors/mod.rs
+++ src/imageboards/extractors/mod.rs
@@ -26,12 +26,13 @@ pub mod moebooru;
 pub trait ImageBoardExtractor {
     /// Searches the tags list on a per-page way. It's relatively the fastest way, but subject to slowdowns since it needs
     /// to iter through all pages manually in order to fetch all posts.
-    fn search<T: Into<String>, I: Unsigned>(
-        &self,
-        tags: &[T],
-        page: I,
-    ) -> Result<Vec<Post>, ExtractorError>;
+    fn search<T, I>(&self, tags: &[T], page: I) -> Result<Vec<Post>, ExtractorError>
+    where
+        T: Into<String>,
+        I: Unsigned;
 
     /// Searches all posts from all pages with given tags, it's the most pratical one, but slower on startup since it will search all pages by itself until it finds no more posts.
-    fn full_search<T: Into<String>>(&self, tags: &[T]) -> Result<Vec<Post>, ExtractorError>;
+    fn full_search<T>(&self, tags: &[T]) -> Result<Vec<Post>, ExtractorError>
+    where
+        T: Into<String>;
 }
