01bc32e516fa4a0796f6ea48b155d1c120e98832
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-25 21:30:34 -0300

    [GBL] write stuff in blacklist file after creating

--- src/imageboards/blacklist.rs
+++ src/imageboards/blacklist.rs
@@ -1,10 +1,13 @@
 use ahash::AHashSet;
 use anyhow::Error;
+use log::debug;
 use serde::{Deserialize, Serialize};
 use tokio::fs::{read_to_string, File};
+use tokio::io::AsyncWriteExt;
 use toml::from_str;
 use xdg::BaseDirectories;
-use log::debug;
+
+const BF_INIT_TEXT: &[u8; 92] = b"# Place in the array all the tags that will be excluded from all imageboards\n\nblacklist = []";
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct GlobalBlacklist {
@@ -17,10 +20,10 @@ impl GlobalBlacklist {
         let xdg_dir = BaseDirectories::with_prefix("imageboard-downloader")?;
 
         let dir = xdg_dir.place_config_file("blacklist.toml")?;
-        
+
         if !dir.exists() {
             debug!("Creating blacklist file");
-            File::create(&dir).await?;
+            File::create(&dir).await?.write_all(BF_INIT_TEXT).await?;
         }
 
         let gbl_string = read_to_string(&dir).await?;
