4a739e83662613f427d27f580065d144254e733e
Ferrah Aiko Wolf <ferrahwolfeh@protonmail.com>
2022-08-27 09:46:28 -0300

    [refactor] progress on downloader trait

--- src/imageboards/extractors/mod.rs
+++ src/imageboards/extractors/mod.rs
@@ -2,12 +2,9 @@
 //! # Extractors
 //!
 //! All modules inside work by connecting to a imageboard website, search for posts with the tags supplied and parse all of them into a `Vec<Post>`
-
-use num_traits::Unsigned;
-
-use crate::Post;
-
 use self::error::ExtractorError;
+use super::queue::PostQueue;
+use async_trait::async_trait;
 
 #[cfg(feature = "danbooru")]
 pub mod danbooru;
@@ -22,17 +19,13 @@ mod error;
 #[cfg(feature = "moebooru")]
 pub mod moebooru;
 
-/// This trait is the only public interface all the extractors should expose
+/// This trait is the only public interface all the extractors should expose aside from some other website-specific configuration.
+#[async_trait]
 pub trait ImageBoardExtractor {
     /// Searches the tags list on a per-page way. It's relatively the fastest way, but subject to slowdowns since it needs
     /// to iter through all pages manually in order to fetch all posts.
-    fn search<T, I>(&self, tags: &[T], page: I) -> Result<Vec<Post>, ExtractorError>
-    where
-        T: Into<String>,
-        I: Unsigned;
+    async fn search(&mut self, page: usize) -> Result<PostQueue, ExtractorError>;
 
     /// Searches all posts from all pages with given tags, it's the most pratical one, but slower on startup since it will search all pages by itself until it finds no more posts.
-    fn full_search<T>(&self, tags: &[T]) -> Result<Vec<Post>, ExtractorError>
-    where
-        T: Into<String>;
+    async fn full_search(&mut self) -> Result<PostQueue, ExtractorError>;
 }
