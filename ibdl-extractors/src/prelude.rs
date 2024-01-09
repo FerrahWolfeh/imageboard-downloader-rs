323004f2a8cb2fd1701d5e1f34833e9b731d196f
Ferrah Aiko Wolf <ferrahwolfeh@proton.me>
2024-01-09 00:25:31 -0300

    remove that deprecated one

--- ibdl-extractors/src/prelude.rs
+++ ibdl-extractors/src/prelude.rs
@@ -2,7 +2,6 @@ pub use crate::imageboards::AsyncFetch;
 pub use crate::imageboards::Auth;
 pub use crate::imageboards::Extractor;
 pub use crate::imageboards::ExtractorThreadHandle;
-pub use crate::imageboards::MultiWebsite;
 pub use crate::imageboards::PoolExtract;
 pub use crate::imageboards::PostFetchAsync;
 pub use crate::imageboards::SinglePostFetch;
